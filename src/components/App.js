1a33d0e8a92ad6baa0865b9bbd0daa3517958df0
Patric Pereira <patric.pereirafranca@gmail.com>
2020-04-25 12:02:56 -0300

    working forms

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,8 @@
 import React from "react";
 import { Route, Switch, Redirect } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+
+import "react-toastify/dist/ReactToastify.css";
 
 import AboutPage from "./AboutPage";
 import CoursesPage from "./CoursesPage";
@@ -11,12 +14,14 @@ import NotFoundPage from "./NotFoundPage";
 function App() {
   return (
     <div className="container-fluid">
+      <ToastContainer autoClose={3000} hideProgressBar />
       <Header />
       <Switch>
         <Route path="/" exact component={HomePage} />
         <Route path="/courses" component={CoursesPage} />
         <Route path="/about" component={AboutPage} />
         <Route path="/course/:slug" component={ManageCoursePage} />
+        <Route path="/course" component={ManageCoursePage} />
         <Redirect from="/about-page" to="about" />
         <Route component={NotFoundPage} />
       </Switch>
