9c4ecadf2b1d86f4f731e872e8b990acb567a91a
Patric Pereira <patric.pereirafranca@gmail.com>
2020-04-25 10:15:35 -0300

    create routes in application

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,25 @@
 import React from "react";
+import { Route, Switch, Redirect } from "react-router-dom";
 
 import AboutPage from "./AboutPage";
 import CoursesPage from "./CoursesPage";
 import Header from "./common/Header";
 import HomePage from "./HomePage";
+import ManageCoursePage from "./ManageCoursePage";
+import NotFoundPage from "./NotFoundPage";
 
 function App() {
-  function getPage() {
-    const route = window.location.pathname;
-    if (route === "/about") return <AboutPage />;
-    if (route === "/courses") return <CoursesPage />;
-    return <HomePage />;
-  }
-
   return (
     <div className="container-fluid">
       <Header />
-      {getPage()}
+      <Switch>
+        <Route path="/" exact component={HomePage} />
+        <Route path="/courses" component={CoursesPage} />
+        <Route path="/about" component={AboutPage} />
+        <Route path="/course/:slug" component={ManageCoursePage} />
+        <Redirect from="/about-page" to="about" />
+        <Route component={NotFoundPage} />
+      </Switch>
     </div>
   );
 }
